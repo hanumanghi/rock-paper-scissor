4c832ac04d870212ec134c46b7ac798b9dac08e8
hanumanghi <drewpatel97@gmail.com>
2022-07-21 14:01:58 -0500

    Added code to  javascript to make the buttons give an input

--- java.js
+++ java.js
@@ -1,37 +1,40 @@
 
 const playerscore = document.querySelector('.presult');
 const computerscore = document.querySelector('.cresult');
-const playeroptions = document.querySelectorAll('button');
+const playeroptions = document.querySelectorAll('.selection');
 
-playeroptions.forEach (playeroption => playeroption.addEventListener('click', e => {
-    playerDecision = (e.target.id);
-}))
+playeroptions.forEach (playeroption => playeroption.addEventListener('click', decision))
 
 function computerPlay(){
     let choices = ['rock', 'paper', 'scissor']
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-let computerSelection = computerPlay();
-let playerSelection = playerDecision;
 let playerRound = 0;
 let computerRound = 0;
-console.log(playerSelection, computerSelection)
+
 function playRound(playerSelection, computerSelection){
-     
+
     if ((playerSelection == 'rock' && computerSelection == 'scissor')
-       || (playerSelection == 'paper' && computerSelection == 'rock')
-       || (playerSelection == 'scissor' && computerSelection == 'paper'))
+     || (playerSelection == 'paper' && computerSelection == 'rock')
+     || (playerSelection == 'scissor' && computerSelection == 'paper'))
        {
            playerscore.textContent = ++playerRound;
-           return "Nice Job!" || "You Won"; 
-       }
-    else { computerscore.textContent = ++computerRound;
-        return  "Try Again!" || "OH NO!"};
-       
-    if (computerSelection == playerSelection)
-         {
-          return  "It\'s a tie"
+           return 'Nice Job!' + playerSelection + 'beats' + computerSelection; 
+       }   
+    else if (playerSelection == computerSelection){
+        return "It\'s a tie, Try Again!"
+    }
+    else {
+        computerscore.textContent = ++computerRound;
+        return  'Try Again!' + computerSelection +  'beats' + playerSelection;
+    }
         }
 
-        }
+        function decision(e) {
+            let playerSelection = e.target.id;
+            let computerSelection = computerPlay()
+            playRound(playerSelection, computerSelection)
+            console.log(playerSelection, computerSelection)
+            
+           }
